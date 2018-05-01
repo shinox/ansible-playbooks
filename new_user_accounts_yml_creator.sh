ae9caaa2c0817d753c90d3c22041a8a43c761d69
MrM <daniel.mikolajczyk@kcl.ac.uk>
2018-05-01 12:05:21 +0100

    Changes to ansible yml notation

--- new_user_accounts_yml_creator.sh
+++ new_user_accounts_yml_creator.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 #
 # MrM: 
-# 25 Jul 2017
+# 25 Jul 2017 - modified:
+#			- 01/05/2018 - minor correction to spacing and ansible yml notation
 #
-# WORK IN PROGRESS DO NOT USE UNLESS YOU ARE THE CREATOR !!!
 # 
 # -- Script add users from ../users.txt file to the server and 
 #    creates ansible script so it can be populated to clients
 # -- Gotchas:
 #    1.) Check the uid:guid start from
 #    2.) point to list with names and uid to start from
-#    3.) Edit USER_GROUPS to include required outcome
+#    3.) Edit MEMBER_OF_GROUPS to include required membership
 #    4.) .yml ansible script is always generated but 
 #        users already present in server configs are not updated,
 #
-PLAYBOOKS="/home/<USER_NAME>/ansible/playbooks"
+PLAYBOOKS="/home/<username>/ansible/playbooks"
 # Get list from file then just apply to server and generate .yml "butter on bread"
 FILE=$1
 
@@ -22,21 +22,25 @@ l_uid=$2
 # Stamp when script run
 DATE=`date +%H%M_%d%m%Y`
 
-USER_GROUPS="hive"
+MEMBER_OF_GROUPS="<group1>,<group2>"
+
 
 ## get length of an array
 # groupslength=${#groups[@]}
 #stafflength=${#staff[@]}
 
+## What uid is starting point - inclusive, 
+## note someone clever populated uid 1021, 1022 hence this script will fail to work, need re-think the idea, 
+
 # Get header before loop
 #read -d '' yml_header_block <<"YML_HEADER_BLOCK"
-cat <<EOF >> $PLAYBOOKS/new_USER_users_$DATE.yml
+cat <<EOF >> $PLAYBOOKS/new_GROUP_users_$DATE.yml
 ---
-- hosts: hive
-
+- hosts: all
   tasks:
-
 EOF
+#YML_HEADER_BLOCK
+#echo "$yml_header_block" >> $PLAYBOOKS/new_GROUP_users_$DATE.yml
 #
 ##
 #
@@ -46,7 +50,7 @@ i=0
 while IFS= read -r lname;
 do  
  #
- #
+ ### FOR SOME REASON SECOND THIRD ETC USER NAMES ARE WRONGLY FORMATTED
  #
  # sname=$(echo $lname | awk '$0=$0FS {print $5, tolower(substr($1,1,1)$NF)}' | sed -e 's/^ //g')
   sname=$(echo $lname | awk '$0=$0FS {print $5, tolower(substr($1,1,1)$NF)}')
@@ -58,14 +62,16 @@ do
   if [ ! `id -u $sname 2>/dev/null || echo -1` -ge 0 ]; then
       echo "User not found will add:"
       groupadd -g $l_uid $sname
-      useradd -g $l_uid -u $l_uid -c "$l_name" -m -s/bin/bash -G$USER_GROUPS $sname
+      useradd -g $l_uid -u $l_uid -c "$l_name" -m -s/bin/bash -G$MEMBER_OF_GROUPS $sname
       echo -e "$RPASS\n$RPASS" | passwd $sname
   else
       echo "User exists want to change pass to random?";
       echo "Pass will match .yml config";
+## 
       #
       # This line is culprit, the stdin is already taken by while do < redirect
-      # hence need to address tty directly,     
+      # hence need to address tty directly, also explains why every first letter of name was missing
+      # but only after first iteration onwards :)
       #
       read -n 1 -p "Y/n:" pass < /dev/tty
       case ${pass:0:1} in
@@ -81,25 +87,25 @@ do
      
   fi
 
-# Create .yml script anyway cause we can
-cat <<EOF >> $PLAYBOOKS/new_USER_users_$DATE.yml
-  - name: create group for user: ${sname}
+# Create .yml script anyway cause we can, VARS NOT POPULATE FIX !!!
+cat <<EOF >> $PLAYBOOKS/new_GROUP_users_$DATE.yml
+  - name: create group for user ${sname}
     group:
         name: ${sname}
         gid: ${l_uid}
         state: present
 
-  - name: create new NIG account: ${sname}
+  - name: create new GROUP account ${sname}
     user:
-        name=${sname}
-        comment=${lname}
-        password="{{ '${RPASS}' | password_hash('sha512') }}"
-        shell=/bin/bash
-        group=${sname}
-        groups=${USER_GROUPS}
-        uid=${l_uid}
-        update_password=always
-        state=present
+        name: ${sname}
+        comment: ${lname}
+        password: "{{ '${RPASS}' | password_hash('sha512') }}"
+        shell: /bin/bash
+        group: ${sname}
+        groups: ${MEMBER_OF_GROUPS}
+        uid: ${l_uid}
+        update_password: always
+        state: present
 
 EOF
 
@@ -110,6 +116,9 @@ sleep 1
 i=$(($i +1 ))
 lname=""
 sname=""
+# 
 #
 done < $FILE
+#
+## If You dare you can even execute ansible but this is trivial hence left for man job,
 #
